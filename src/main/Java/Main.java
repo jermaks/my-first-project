4b6844353cd8da353272f2fc25ecea31fa285076
Arturs Jermaks <arthurjermak@gmail.com>
2023-02-22 20:07:25 +0200

    [main]: Upload project

---
+++ src/main/Java/Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+
+        String hello = "Hello";
+        String world = "World";
+        System.out.println(hello + " " + world + "!");
+    }
+}
